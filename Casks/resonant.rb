77ddf224e4828a9bc5b39ee28dfd8aad2de3140e
TMS <t@schlossmacher.at>
2026-03-30 14:12:32 +0200

    Add Resonant cask (v0.1.63)

---
+++ Casks/resonant.rb
@@ -0,0 +1,20 @@
+cask "resonant" do
+  version "0.1.63"
+  sha256 "e4a86edf4494567cc0d6a483f435c7ae5b406e6acf2a6e83abdd956a366081e2"
+
+  url "https://downloads.onresonant.com/Resonant-latest-arm64.dmg"
+  name "Resonant"
+  desc "Voice-first productivity app for macOS"
+  homepage "https://onresonant.com"
+
+  depends_on macos: ">= :sonoma"
+  depends_on arch: :arm64
+
+  app "Resonant.app"
+
+  zap trash: [
+    "~/Library/Application Support/Resonant",
+    "~/Library/Caches/com.resonance.resonant",
+    "~/Library/Preferences/com.resonance.resonant.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# Homebrew Tap for Resonant
+
+## Install
+
+```sh
+brew tap resonantcom/tap
+brew install --cask resonant
+```
+
+## Update
+
+```sh
+brew upgrade --cask resonant
+```
+
+## Uninstall
+
+```sh
+brew uninstall --cask resonant
+```
