5a27407a603d58028d76873673d4a0492e1a6a0f
TMS <t@schlossmacher.at>
2026-05-04 19:55:57 +0200

    Update Resonant to 0.1.77

--- Casks/resonant.rb
+++ Casks/resonant.rb
@@ -2,7 +2,7 @@ cask "resonant" do
   version "0.1.77"
   sha256 "c6d03470bf035a7b83314a105f3ac2e6bbe1cbc0c61ac1a2bb2f426b7e3e8a0b"
 
-  url "https://downloads.onresonant.com/Resonant-latest-arm64.dmg"
+  url "https://downloads.onresonant.com/Resonant-0.1.77-build-20260501095115-arm64.dmg"
   name "Resonant"
   desc "Voice-first productivity app for macOS"
   homepage "https://onresonant.com"
