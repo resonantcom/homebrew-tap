da956e71a880f1a0c418dcfbea58962a10bcd190
TMS <your-email@example.com>
2026-06-17 14:14:34 +0200

    Update Resonant to 0.1.82

--- Casks/resonant.rb
+++ Casks/resonant.rb
@@ -1,8 +1,8 @@
 cask "resonant" do
   version "0.1.82"
-  sha256 "c6a5170fcdff5d48577a30ffb8aa14322f5eb43aef85fbbe477634d1a5b518b3"
+  sha256 "55bb13a979b0c7b64e0fce8257bea72f37be8c9fa54a5e38c3207c293b7cd44c"
 
-  url "https://downloads.onresonant.com/Resonant-0.1.82-build-20260616093615-arm64.dmg"
+  url "https://downloads.onresonant.com/Resonant-0.1.82-build-20260617120749-arm64.dmg"
   name "Resonant"
   desc "Voice-first productivity app for macOS"
   homepage "https://onresonant.com"
